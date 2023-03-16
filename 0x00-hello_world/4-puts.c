30a1d54d43df103cb70a29a6a55ede1f08265c46
Braa33 <braafadlrabie@gmail.com>
2023-03-16 15:38:11 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: A C Program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+ */
+
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
