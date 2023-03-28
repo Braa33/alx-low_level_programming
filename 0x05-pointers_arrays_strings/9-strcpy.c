bff9ad1da4e0950ae0c36fd2993f1e7aa2a0e33d
Braa33 <braafadlrabie@gmail.com>
2023-03-27 17:50:03 -0700

    l

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,13 @@
  * @src: char type string
  * Eescription: Copy the string ointed to by ointer 'src' to
  * Return: pointer to 'dest'
- */
+*/
 
 char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 	
-	do{
+	do {
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
